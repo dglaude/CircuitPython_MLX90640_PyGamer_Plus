0ee45c115e01e1e081c99269a797596304728adc
BuildTools <david.glaude@gmail.com>
2020-02-08 16:53:24 +0100

    replace custom palette to using fancyled library to build the gradian.

--- mlx90640_pygamer_plus.py
+++ mlx90640_pygamer_plus.py
@@ -8,38 +8,23 @@ import displayio
 import terminalio
 from adafruit_display_text.label import Label
 from simpleio import map_range
+import adafruit_fancyled.adafruit_fancyled as fancy
 
 number_of_colors = 64                          # Number of color in the gradian
 last_color = number_of_colors-1                # Last color in palette
 palette = displayio.Palette(number_of_colors)  # Palette with all our colors
 
-## Heatmap code inspired from: http://www.andrewnoske.com/wiki/Code_-_heatmaps_and_color_gradients
-color_A = [[0, 0, 0], [0, 0, 255], [0, 255, 255], [0, 255, 0], [255, 255, 0], \
-    [255, 0, 0], [255, 255, 255]]
-color_B = [[0, 0, 255], [0, 255, 255] , [0, 255, 0], [255, 255, 0], [255, 0, 0]]
-color_C = [[0, 0, 0], [255, 255, 255]]
-color_D = [[0, 0, 255], [255, 0, 0]]
+# gradian for fancyled palette generation
+grad = [(0.00, fancy.CRGB(0, 0, 255)),    # Blue
+        (0.25, fancy.CRGB(0, 255, 255)),
+        (0.50, fancy.CRGB(0, 255, 0)),    # Green
+        (0.75, fancy.CRGB(255, 255, 0)),
+        (1.00, fancy.CRGB(255, 0, 0))]    # Red
 
-color = color_B
-NUM_COLORS = len (color)
-
-def MakeHeatMapColor():
-    for c in range(number_of_colors):
-        value = c * (NUM_COLORS-1) / last_color
-        idx1  = int(value)              # Our desired color will be after this index.
-        if idx1 == value :              # This is the corner case
-            red   = color[idx1][0]
-            green = color[idx1][1]
-            blue  = color[idx1][2]
-        else:
-            idx2  = idx1+1                  # ... and before this index (inclusive).
-            fractBetween = value - idx1     # Distance between the two indexes (0-1).
-            red   = int(round((color[idx2][0] - color[idx1][0]) * fractBetween + color[idx1][0]))
-            green = int(round((color[idx2][1] - color[idx1][1]) * fractBetween + color[idx1][1]))
-            blue  = int(round((color[idx2][2] - color[idx1][2]) * fractBetween + color[idx1][2]))
-        palette[c]= ( 0x010000 * red ) + ( 0x000100 * green ) + ( 0x000001 * blue )
-
-MakeHeatMapColor()
+# create our palette using fancyled expansion of our gradian
+fancy_palette = fancy.expand_gradient(grad, number_of_colors)
+for c in range(number_of_colors):
+    palette[c] = fancy_palette[c].pack()
 
 # Bitmap for colour coded thermal value
 image_bitmap = displayio.Bitmap( 32, 24, number_of_colors )
@@ -50,6 +35,7 @@ image_group = displayio.Group(scale=4)
 image_group.append(image_tile)
 
 scale_bitmap = displayio.Bitmap( number_of_colors, 1, number_of_colors )
+
 # Create a Group Scale must be 128 divided by number_of_colors
 scale_group = displayio.Group(scale=2)
 scale_tile = displayio.TileGrid(scale_bitmap, pixel_shader=palette, x = 0, y = 60)
@@ -80,7 +66,6 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C")
-print([hex(i) for i in mlx.serial_number])
 
 #mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
@@ -91,8 +76,7 @@ while True:
     stamp = time.monotonic()
     try:
         mlx.getFrame(frame)
-    except ValueError:
-        # these happen, no biggie - retry
+    except ValueError:           # these happen, no biggie - retry
         continue
 
 #    print("Time for data aquisition: %0.2f s" % (time.monotonic()-stamp))
@@ -115,5 +99,6 @@ while True:
 
     min_t = mini                  # Automatically change the color scale
     max_t = maxi
+
 #    print((mini, maxi))           # Use this line to display min and max graph in Mu
 #    print("Total time for aquisition and display %0.2f s" % (time.monotonic()-stamp))
